08dcbf8f31ea150e6d5a21cbd65e2e9376ebce28
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-06-24 14:59:46 -0600

    Adding ProductNameBo interface and its implementation

---
+++ src/main/java/com/myretail/business/ProdcutNameBoImpl.java
@@ -0,0 +1,38 @@
+package com.myretail.business;
+
+import org.springframework.stereotype.Component;
+
+import com.myretail.domain.ProductName;
+import com.myretail.model.Result;
+
+@Component
+public class ProdcutNameBoImpl implements ProductNameBo {
+
+	@Override
+	public Result saveProductName(ProductName productName) {
+		return null;
+	}
+
+	@Override
+	public Result updateProductName(ProductName productName) {
+		return null;
+	}
+
+	@Override
+	public Result deleteProductName(ProductName productName) {
+		return null;
+	}
+
+	@Override
+	public Result getProductName(ProductName productName) {
+		return null;
+	}
+
+	@Override
+	public Result getAllProductNames(ProductName productName) {
+		return null;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/myretail/business/ProductNameBo.java
@@ -0,0 +1,18 @@
+package com.myretail.business;
+
+import com.myretail.domain.ProductName;
+import com.myretail.model.Result;
+
+public interface ProductNameBo {
+	
+	public Result saveProductName(ProductName productName);
+	
+	public Result updateProductName(ProductName productName);
+	
+	public Result deleteProductName(ProductName productName);
+	
+	public Result getProductName(ProductName productName);
+	
+	public Result getAllProductNames(ProductName productName);
+
+}

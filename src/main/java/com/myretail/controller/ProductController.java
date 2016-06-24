c010fec52e565798d98f6c65724d1d3bbef40b69
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-06-24 15:22:26 -0600

    Added additional uri's to ProductController

--- src/main/java/com/myretail/controller/ProductController.java
+++ src/main/java/com/myretail/controller/ProductController.java
@@ -1,18 +1,47 @@
 package com.myretail.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myretail.business.ProductNameBo;
+import com.myretail.domain.ProductName;
 import com.myretail.model.Result;
 
 @RestController
 public class ProductController {
+	
+	@Autowired
+	private ProductNameBo productNameBo;
 
 	@RequestMapping(value="/productName", method=RequestMethod.POST)
-	public Result saveProductName(){
-		return null;
+	public Result saveProductName(@RequestBody ProductName productName){
+		return productNameBo.saveProductName(productName);
 	}
 	
+	@RequestMapping(value="/productName", method=RequestMethod.PUT)
+	public Result updateProductName(@RequestBody ProductName productName){
+		return productNameBo.updateProductName(productName);
+	}
+	
+	@RequestMapping(value="/productName", method=RequestMethod.DELETE)
+	public Result deleteProductName(@RequestBody ProductName productName){
+		return productNameBo.deleteProductName(productName);
+	}
+	
+	@RequestMapping(value="/productName/{productId}", method=RequestMethod.GET)
+	public ProductName getProductName(@PathVariable String productId){
+		return productNameBo.getProductName(productId);
+	}
+	
+	@RequestMapping(value="/productName", method=RequestMethod.GET)
+	public ArrayList<ProductName> getAllProductNames(){
+		return productNameBo.getAllProductNames();
+	}
 	
 }

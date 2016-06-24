aec471092de6fd17547ffe34019a01e80122c220
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-06-24 13:31:40 -0600

    Adding /sample service that inserts sample object into mongodb

--- src/main/java/com/myretail/controller/HelloController.java
+++ src/main/java/com/myretail/controller/HelloController.java
@@ -1,14 +1,32 @@
 package com.myretail.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.myretail.domain.Sample;
 
 @RestController
 public class HelloController {
 
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
 	@RequestMapping("/hello")
 	public String sayHello(){
-		return "Hello. This is a sample service by Kartheek Maremalla";
+		return "Hello. This is a sample service by Kartheek Maremalla.";
+	}
+	
+	@RequestMapping("/sample")
+	public String insertSampleDocument(){
+		
+		Sample samp = new Sample();
+		samp.setMessage("This is a sample message from eclipse");
+		samp.setMessageId(1);
+		mongoTemplate.insert(samp);
+		return "Sample Message Insert Successful";
+		
 	}
 	
 }

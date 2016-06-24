c154f4797a0df96130307b22714f7fb2b25b3d07
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-06-24 15:12:23 -0600

    Adding ProductNameDao and implementation

---
+++ src/main/java/com/myretail/dao/ProductNameDaoImpl.java
@@ -0,0 +1,34 @@
+package com.myretail.dao;
+
+import java.util.ArrayList;
+
+import com.myretail.domain.ProductName;
+
+public class ProductNameDaoImpl implements ProductNameDao {
+
+	@Override
+	public void saveProductName(ProductName productName) {
+		
+	}
+
+	@Override
+	public void updateProductName(ProductName productName) {
+		
+	}
+
+	@Override
+	public void deleteProductName(ProductName productName) {
+		
+	}
+
+	@Override
+	public ProductName getProductName(String productId) {
+		return null;
+	}
+
+	@Override
+	public ArrayList<ProductName> getAllProductNames() {
+		return null;
+	}
+
+}

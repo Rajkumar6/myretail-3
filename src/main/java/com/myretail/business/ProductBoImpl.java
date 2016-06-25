c69acec9776113a1d198e9e9d4421f5acbb90de3
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-06-25 15:11:43 -0600

    Added ProductBoImpl

---
+++ src/main/java/com/myretail/business/ProductBoImpl.java
@@ -0,0 +1,34 @@
+package com.myretail.business;
+
+import java.util.ArrayList;
+
+import com.myretail.model.Product;
+import com.myretail.model.Result;
+
+public class ProductBoImpl implements ProductBo {
+
+	@Override
+	public Product getProduct(String productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Product> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result saveProduct(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result updateProduct(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

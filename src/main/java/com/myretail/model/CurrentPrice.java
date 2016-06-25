2f840df0fe51fe910403c8367201c3a58b1779f0
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-06-25 12:28:15 -0600

    Adding models for Product and CurrentPrice

---
+++ src/main/java/com/myretail/model/CurrentPrice.java
@@ -0,0 +1,23 @@
+package com.myretail.model;
+
+import java.math.BigDecimal;
+
+public class CurrentPrice {
+
+	private BigDecimal value;
+	private Currency currency;
+	
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	public Currency getCurrency() {
+		return currency;
+	}
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+	
+}
---
+++ src/main/java/com/myretail/model/Product.java
@@ -0,0 +1,30 @@
+package com.myretail.model;
+
+public class Product {
+	
+	private String productId;
+	private String productName;
+	private CurrentPrice currentPrice;
+	
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public CurrentPrice getCurrentPrice() {
+		return currentPrice;
+	}
+	public void setCurrentPrice(CurrentPrice currentPrice) {
+		this.currentPrice = currentPrice;
+	}
+	
+	
+
+}

cd4052d1a48713db9ae38cd5f64bbad7d7561139
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-06-25 15:08:29 -0600

    Adding ProductBo Interface

---
+++ src/main/java/com/myretail/business/ProductBo.java
@@ -0,0 +1,18 @@
+package com.myretail.business;
+
+import java.util.ArrayList;
+
+import com.myretail.model.Product;
+import com.myretail.model.Result;
+
+public interface ProductBo {
+	
+	public Product getProduct(String productId);
+	
+	public ArrayList<Product> getAllProducts();
+	
+	public Result saveProduct(Product product);
+	
+	public Result updateProduct(Product product);
+
+}

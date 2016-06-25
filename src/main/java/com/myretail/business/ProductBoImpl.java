8a30b524706802dcae08e10970af848e2815b10e
Kartheek Maremalla <kmaremalla@DW20051USSLM.local>
2016-06-25 15:15:50 -0600

    Deleting ProductDao Interface

--- src/main/java/com/myretail/business/ProductBoImpl.java
+++ src/main/java/com/myretail/business/ProductBoImpl.java
@@ -2,11 +2,17 @@ package com.myretail.business;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myretail.dao.ProductDao;
 import com.myretail.model.Product;
 import com.myretail.model.Result;
 
+@Service
 public class ProductBoImpl implements ProductBo {
 
+	
 	@Override
 	public Product getProduct(String productId) {
 		// TODO Auto-generated method stub
